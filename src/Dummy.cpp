a1afb23cbb733b9a0c6a3e84b091f123a2f414a1
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2015-03-30 16:10:31 -0300

    Initial Commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace gpu_sonar_simulation;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <gpu_sonar_simulation/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    gpu_sonar_simulation::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
