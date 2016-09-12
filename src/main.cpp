9bc7a4847876486512e282d28eee6568e79075e7
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2016-09-12 13:01:25 -0300

    add mean and stddev to performance test

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <numeric>
 
 // Rock includes
 #include <gpu_sonar_simulation/Sonar.hpp>
@@ -28,31 +29,49 @@ cv::Mat createRandomImage(int rows, int cols) {
 int main(int argc, char* argv[])
 {
     /* generate a random shader image */
-    uint width = 5144;
+    uint width = 3352;
     base::Angle beam_width = base::Angle::fromDeg(120.0);
     base::Angle beam_height = base::Angle::fromDeg(20.0);
     uint height = width * tan(beam_height.rad * 0.5) / tan(beam_width.rad * 0.5);
     cv::Mat cv_shader = createRandomImage(height, width);
 
-    base::Time ts0 = base::Time::now();
-
     /* initialize Sonar Simulation */
-    uint32_t bin_count = 500;
+    uint32_t bin_count = 1000;
     uint32_t beam_count = 256;
     Sonar sonar_sim(bin_count, beam_count, beam_width, beam_height);
 
-    /* simulate sonar image */
-    std::vector<float> bins;
-    sonar_sim.decodeShader(cv_shader, bins);
+    /* process 200 times */
+    std::vector<double> timestamp;
+    for (size_t i = 0; i < 200; i++) {
+        base::Time ts0 = base::Time::now();
 
-    /* apply additional gain */
-    float gain = 0.5;
-    sonar_sim.applyAdditionalGain(bins, gain);
+        /* simulate sonar image */
+        std::vector<float> bins;
+        sonar_sim.decodeShader(cv_shader, bins);
 
-    /* encapsulate in rock's sonar structure */
-    base::samples::Sonar sonar = sonar_sim.simulateSonar(bins, 50);
+        /* apply additional gain */
+        float gain = 0.5;
+        sonar_sim.applyAdditionalGain(bins, gain);
 
-    std::cout << "Time: " << (base::Time::now() - ts0).toSeconds() << std::endl;
+        /* encapsulate in rock's sonar structure */
+        float range = 50.0;
+        base::samples::Sonar sonar = sonar_sim.simulateSonar(bins, range);
+
+        /* accumulate timestamps */
+        timestamp.push_back((base::Time::now() - ts0).toSeconds());
+    }
+
+    /* Performance results */
+    double sum = std::accumulate(timestamp.begin(), timestamp.end(), 0.0);
+    double mean = sum / timestamp.size();
+
+    double accum = 0.0;
+    for (size_t i = 0; i < timestamp.size(); i++)
+        accum += (timestamp[i] - mean) * (timestamp[i] - mean);
+    double stddev = sqrt(accum / (timestamp.size() - 1));
+
+    std::cout << "=== MEAN  : " << mean << std::endl;
+    std::cout << "=== STDDEV: " << stddev << std::endl;
 
     return 0;
 }
