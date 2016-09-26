271d294a42737b472d9c27f1cc7e5a16b5da5ba8
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2016-09-26 20:30:47 -0300

    adapt convertOSG2CV method to process shader and depth buffers
    adapt convertOSG2CV method to process shader and depth buffers

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -9,9 +9,16 @@ using namespace osg;
 namespace gpu_sonar_simulation {
 
 void convertOSG2CV(osg::ref_ptr<osg::Image>& osg_image, cv::Mat& cv_image) {
-    cv_image.create(osg_image->t(), osg_image->s(), CV_32FC3);
-    cv_image.data = osg_image->data();
-    cv::cvtColor(cv_image, cv_image, cv::COLOR_RGB2BGR);
-    cv::flip(cv_image, cv_image, 0);
+    // process shader with 3 channels and 8-bit data
+    if (osg_image->getPixelFormat() == GL_RGB) {
+        cv_image = cv::Mat(osg_image->t(), osg_image->s(), CV_32FC3, osg_image->data());
+        cv::cvtColor(cv_image, cv_image, cv::COLOR_RGB2BGR);
+    }
+
+    // process depth buffer with 1 channel and 32-bit data
+    else {
+        cv_image = cv::Mat(osg_image->t(), osg_image->s(), CV_32FC1, osg_image->data());
+        cv_image = cv_image.mul( cv::Mat1f(cv_image < 1) / 255);
+    }
 }
 }
